2c2c0c535c06ae59b12170aade35f28748ae3da6
S1mkin <s.a.s.s.e.r@mail.ru>
2019-10-21 14:57:34 +0300

    Add vuex example

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -1,16 +1,51 @@
 <template>
-  <div class="hello">
+  <div class="chat_wrapper">
     <h1>{{ msg }}</h1>
+
+    <ul>
+      <li v-for="(msg, key) in get_msg" :key="key">{{msg}}</li>
+    </ul>
+    
+    <input type="text" placeholder="Enter new message" v-model="input_msg" />
+    <button @click="add_msg">Add message</button>
+
   </div>
 </template>
 
+
+
 <script>
+
+import {mapGetters} from 'vuex'
+// import {mapActions} from 'vuex'
+// import {mapMutations} from 'vuex'
+
 export default {
   name: 'chat',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      input_msg: ''
+    }
+  },
+  computed: {
+    ...mapGetters(['get_msg']),
+  },
+  methods: {
+    // ...mapActions(['add_new_msg']),
+    // ...mapMutations(['add_msg']),
+    add_msg(){
+      // вызов store.mutations - add_msg
+      //this.$store.commit('add_msg');
+
+      // вызов store.actions - add_new_msg
+      this.$store.dispatch('add_new_msg', { msg: this.input_msg});
+    }
   }
 }
+
 </script>
 
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Chat from './modules/chat'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    Chat
   }
 })
